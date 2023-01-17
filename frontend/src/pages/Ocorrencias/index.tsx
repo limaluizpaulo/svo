14d813a9e0a56d20e4d3a36a47450e3ff90d6e81
Luiz Paulo Lima <luiz.lima@comuniki.me>
2023-01-17 20:49:08 -0300

    Tooltip

--- frontend/src/pages/Ocorrencias/index.tsx
+++ frontend/src/pages/Ocorrencias/index.tsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from "react";
+import { Tooltip } from 'react-tooltip'
+import 'react-tooltip/dist/react-tooltip.css'
 import { Link } from "react-router-dom";
 import api from "../../api/server";
 import moment from 'moment';
-import { useCadastro } from "../../context/cadastro";
-
+import { useSvo } from "../../context/svo";
+import { toast } from "react-toastify";
 
 export default function Ocorrencias() {
-  const thead = ["#", "IML", "Nome", "BO", "Chamado", "Status", "Endereço", "Ações"];
-  const { ocorrencias, falecidos, enderecos } = useCadastro()
+  const thead = ["Pericia", "Nome", "Chamado", "Status", "Endereço", "BO", "Ações"];
+  const { ocorrencias, falecidos, enderecos } = useSvo()
+  const [order, setOrder] = useState("asc");
+
+  const o = order === "asc" ? (
+    (a, b) => a.numeroControle > b.numeroControle ? -1 : a.numeroControle < b.numeroControle ? 1 : 0) : (
+    (a, b) => a.numeroControle < b.numeroControle ? -1 : a.numeroControle > b.numeroControle ? 1 : 0
+  )
+
+
 
   function getFalecido(id: number) {
     const falecido = falecidos.find((falecido) => falecido.id === id);
@@ -26,25 +36,43 @@ export default function Ocorrencias() {
         Authorization: `Bearer ${localStorage.getItem("token")!}`,
       },
     });
-    alert("Data deleted");
+    toast.info("Data deleted");
     // window.location.reload();
   }
 
+
+
+
+
+
   function getStatus(entrada: string, liberacao: string) {
+    if (liberacao) {
+      return (
+        <>
+          <button id="liberado" className="btn btn-sm btn-success"  >
+            Liberado
+          </button>
+          <Tooltip anchorId="liberado" variant="info" place="top"
+            content={`Liberado dia ${moment(liberacao).format('DD/MM')} às ${moment(liberacao).format('HH:mm')}`} />
+        </>
+      );
+    }
+
     if (entrada) {
-      return <button className="btn btn-sm btn-primary"
-        title={`Chegou dia ${moment(entrada).format("DD/MM")}
-      às ${moment(entrada).format("HH:mm")}`}
-      >Chegou</button>;
-    } else if (liberacao) {
-      return <button
-        title={`Liberado dia ${moment(liberacao).format("DD/MM")}
-      às ${moment(liberacao).format("HH:mm")}`}
-        className="btn btn-sm btn-success">Liberado</button>;
+      return (
+        <>
+          <button id="chegou" className="btn btn-sm btn-primary"  >
+            Chegou
+          </button>
+          <Tooltip anchorId="chegou" variant="info" place="top" content={`Chegou dia ${moment(entrada).format('DD/MM')} às ${moment(entrada).format('HH:mm')}`} />
+        </>
+      );
     } else {
       return <button className="btn btn-sm btn-warning">Não Chegou</button>;
     }
   }
+
+
 
   return (
     <div className="mx-5 px-5">
@@ -59,6 +87,9 @@ export default function Ocorrencias() {
       <table className="table border">
         <thead>
           <tr>
+            <th scope="col" onClick={() => setOrder(order === "asc" ? "desc" : "asc")}>
+              <i className={`fas fa-sort-${order === "asc" ? "up" : "down"}`}></i>
+            </th>
             {thead.map((data, index) => (
               <th scope="col" key={index}>
                 {data}
@@ -67,38 +98,42 @@ export default function Ocorrencias() {
           </tr>
         </thead>
         <tbody>
-          {ocorrencias.map((data, index) => (
-            <tr key={data.id}>
-              <th scope="row">{data.numeroControle}/{data.ano.toString().substring(2)}</th>
-              <td>{data.natureza === 1 ? "SVO" : "IML"}</td>
-              <td className="text-nowrap">{getFalecido(Number(data.falecido_id))}</td>
-              <td>{data.numeroBO}</td>
-              <td>{moment(data.dataHoraChamado).format("DD/MM/YYYY HH:mm")}</td>
-              <td>{getStatus(data.dataHoraEntrada, data.dataHoraLiberacao)}</td>
+          {ocorrencias.sort(o
 
-              <td className="text-nowrap
+          ).
+            map((data, index) => (
+              <tr key={data.id}>
+                <th scope="row">{data.numeroControle}/{data.ano.toString().substring(2)}</th>
+                <td>{data.natureza === 1 ? "SVO" : "IML"}</td>
+                <td className="text-nowrap">{getFalecido(Number(data.falecido_id))}</td>
+
+                <td>{moment(data.dataHoraChamado).format("DD/MM/YYYY HH:mm")}</td>
+                <td>{getStatus(data.dataHoraEntrada, data.dataHoraLiberacao)}</td>
+
+                <td className="text-nowrap
               ">{getEndereco(Number(data.endereco_id))}</td>
+                <td>{data.numeroBO}</td>
 
-              <td>
-                <Link
-                  className="text-decoration-none text-primary"
-                  to={`/edit-product/${data.id}`}
-                >
-                  Editar
-                </Link>{" "}
-                |{" "}
-                <span
-                  role="button"
-                  className="text-danger"
-                  onClick={() => deleteData(data.id)}
-                >
-                  Deletar
-                </span>
-              </td>
-            </tr>
-          ))}
+                <td>
+                  <div className=" d-flex justify-content-center
+                ">
+                    <Link
+                      className="btn btn-primary"
+                      to={`/edit-product/${data.id}`}
+                    >
+                      <i className="fas fa-edit"></i>
+                    </Link>{" "}
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => deleteData(data.id)}
+                    >
+                      <i className="fas fa-trash-alt"></i>
+                    </button></div>
+                </td>
+              </tr>
+            ))}
         </tbody>
       </table>
-    </div>
+    </div >
   );
 }
